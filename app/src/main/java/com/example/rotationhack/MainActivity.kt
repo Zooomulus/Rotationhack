90244046e791fc82dd1e37960e6999b2dfe7494b
tweeb_kr8x5ge <andelisun@gmail.com>
2023-03-05 15:49:37 -0500

    commit!

--- app/src/main/java/com/example/rotationhack/MainActivity.kt
+++ app/src/main/java/com/example/rotationhack/MainActivity.kt
@@ -1,16 +1,66 @@
 package com.example.rotationhack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var myButton: Button
+    private lateinit var textView: TextView
+    private lateinit var fadeOutAnimation: Animation
+    private var textIndex = 0
+
+    private val textList = listOf("Hello, Icarus", "Welcome to Earth")
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val runnable = object : Runnable {
+        override fun run() {
+            updateText()
+            handler.postDelayed(this, 10000) // Change delay here (in milliseconds)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView = findViewById<TextView>(R.id.textView)
-        val anim = AnimationUtils.loadAnimation(this, R.anim.computer_text_anim)
-        textView.startAnimation(anim)
+
+        myButton = findViewById(R.id.button)
+        myButton.setOnClickListener {
+            // Do something when button is clicked
+        }
+
+        textView = findViewById(R.id.textView)
+        fadeOutAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+
+        handler.post(runnable)
+    }
+
+    fun updateText() {
+        textView.startAnimation(fadeOutAnimation)
+        fadeOutAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {}
+            override fun onAnimationRepeat(animation: Animation?) {}
+            override fun onAnimationEnd(animation: Animation?) {
+                textView.text = textList[textIndex]
+                textIndex = (textIndex + 1) % textList.size
+                textView.startAnimation(
+                    AnimationUtils.loadAnimation(
+                        this@MainActivity,
+                        R.anim.text_scale
+                    )
+                )
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        handler.removeCallbacks(runnable)
+        super.onDestroy()
     }
 }
+
---
+++ app/src/main/java/com/example/rotationhack/Mainscreen.kt
@@ -0,0 +1,11 @@
+package com.example.rotationhack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Mainscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.screentwo)
+    }
+}
